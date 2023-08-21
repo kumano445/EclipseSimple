71123700352b405f758c8a2fcf539a5606a36589
yoshiko <ttkmysk021@gmail.com>
2023-08-21 12:45:43 +0900

    initial commit

---
+++ expcise/src/expcise/first.java
@@ -0,0 +1,11 @@
+package expcise;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Eclipseを始めよう!");
+		System.out.println(4 + 5);
+	}
+
+}
